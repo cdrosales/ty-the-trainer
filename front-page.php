ace19abf3f07c798b1efac501bb377ff8e526f52
cdrosales <cara10denise@hotmail.com>
2020-04-04 22:09:55 -0400

    test: contact form 7 via mailchimp and mailchimp form builder

--- front-page.php
+++ front-page.php
@@ -16,4 +16,24 @@
         <p>No posts found</p>
 <?php endif; ?>
 
+
+<!-- Begin Mailchimp Signup Form -->
+
+
+<div id="mc_embed_signup">
+<form action="https://tythetrainer.us18.list-manage.com/subscribe/post?u=c8821d49dfda29df2614af2be&amp;id=1af4e15eaf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target=" " novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Let's stay connected</label>
+    <p>Sign up to receive the latest updates and exclusive offers.</p>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c8821d49dfda29df2614af2be_1af4e15eaf" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
+
 <?php get_footer(); ?>
