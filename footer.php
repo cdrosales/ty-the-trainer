34f41dffc9890868d09bca776f8cab89c25e7fe3
cdrosales <cara10denise@hotmail.com>
2020-04-09 05:43:34 -0400

    style: add styles to footer

--- footer.php
+++ footer.php
@@ -2,24 +2,26 @@
 
 
 <footer>
-    <div class="footer-containter">
-        <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/ty-logo-black.png'; ?>" class="footer-logo"/></a>
+    
+    <div class="footer-container">
+
+        <div class="footer-logo-wrapper">
+            <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/ty-logo-black.png'; ?>" class="footer-logo"/></a>
+        </div>
 
         <div class="footer-nav">
             <?php wp_nav_menu( array('theme_location' => 'footer')); ?>
         </div>
 
         <div class="footer-social-media">
-            <ul>
-                <li><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-instagram.svg'; ?>" class="icon-instagram"/></a></li>
-                <li><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-facebook.svg'; ?>" class="icon-facebook"/></a></li>
-                <li><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-linkedin.svg'; ?>" class="icon-linkedin"/></a></li>
-            </ul>
+                <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-instagram.svg'; ?>" class="icon-instagram"/></a>
+                <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-facebook.svg'; ?>" class="icon-facebook"/></a>
+                <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-linkedin.svg'; ?>" class="icon-linkedin"/></a>
         </div>
+
     </div>
 
     <p class="tythetrainer-copyright">©2020 Ty The Trainer Inc.</p>
-
 
 </footer>
 
