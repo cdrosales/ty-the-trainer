0f73bf368779bc697b748d87f71c5249b6b9fbf4
cdrosales <cara10denise@hotmail.com>
2020-04-09 03:20:27 -0400

    style: add container to hero on about/resources/contact

--- page-contact.php
+++ page-contact.php
@@ -5,19 +5,21 @@
     while ( have_posts() ):
         the_post();?> 
 
-<div class="hero-wrapper">
-    <div class="hero-img">
-        <?php the_post_thumbnail( 'full' );  ?>
-    </div>
+<div class="hero-container">
+    <div class="hero-wrapper">
+        <div class="hero-img">
+            <?php the_post_thumbnail( 'full' );  ?>
+        </div>
 
-    <div class="hero-title">
-        <?php the_content(); ?>
- 
-        <?php endwhile; ?> 
+        <div class="hero-title">
+            <?php the_content(); ?>
+    
+            <?php endwhile; ?> 
 
-        <?php else : ?>
-                <p>No posts found</p>
-        <?php endif; ?>
+            <?php else : ?>
+                    <p>No posts found</p>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
 
