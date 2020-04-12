76ddd9b9394fc4d2b7c5944eb7abcb365f15002b
cdrosales <cara10denise@hotmail.com>
2020-04-12 08:10:33 -0400

    styles: add styles for instagram feed section

--- front-page.php
+++ front-page.php
@@ -106,23 +106,39 @@
 
 <section class="instagram-feed">
 
-<div class="widget-title">
+    <div class="widget-title">
         <h2> Instagram</h2>
         <hr class="header-line">
     </div>
 
-<?php if( have_posts() ): 
-while ( have_posts() ):
-    the_post();?> 
 
-<?php the_content(); ?>
+    <div class="instagram-feed-wrapper">
 
-    
-<?php endwhile; ?> 
+        <div class="instagram-feed-info">
+            <h3>@Ty.thetrainer</h3>
+            <p>Follow me on Instagram! From daily motivational quotes and 
+                tutorials to some crazy workout videos. See what my journey 
+                is all about, whether its fitness related or every day life.    
+            </p>
+            <button>Follow on instagram</button>
+        </div>  
 
-<?php else : ?>
-    <p>No posts found</p>
-<?php endif; ?>
+        <div class="instagram-feed-container">
+            <?php if( have_posts() ): 
+            while ( have_posts() ):
+                the_post();?> 
+
+            <?php the_content(); ?>
+
+                
+            <?php endwhile; ?> 
+
+            <?php else : ?>
+                <p>No posts found</p>
+            <?php endif; ?>
+        </div>
+
+    </div>
 
 </section>
 
