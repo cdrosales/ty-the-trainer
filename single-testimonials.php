2db7e99b90087ce65833cb01c89acc2caf8320d7
cdrosales <cara10denise@hotmail.com>
2020-04-16 11:45:52 -0400

    style: add styles to single testimonials page

--- single-testimonials.php
+++ single-testimonials.php
@@ -1,15 +1,24 @@
 <?php get_header('black'); ?>
 
 
-    <div class="page-content">
+    <div class="page-content testimonial-single-container">
         <?php if( have_posts() ): 
         while ( have_posts() ):
             the_post();?> 
+        
+        <div class="testimonials-singe-info-container">
 
-            <h2><?php the_title(); ?></h2> 
+            <div class="testimonials-single-img">
+                <?php the_post_thumbnail( 'full' );  ?>
+            </div>
 
+            <div class="testimonials-single-info">
 
-            <?php the_post_thumbnail( 'full' );  ?>
+                <h3><?php the_title(); ?></h3> 
+                <h4><?php echo get_the_excerpt(); ?></h4>
+
+            </div>
+        </div>
 
                 <?php the_content(); ?>
         
