ad2a395ab8c82b114fc5093356a4adaed32b6022
cdrosales <cara10denise@hotmail.com>
2020-04-21 08:49:45 -0400

    style: add styles to font/inputs/radio fro macro tool form

--- page-resources.php
+++ page-resources.php
@@ -1,21 +1,24 @@
 <?php get_header('black'); ?>
 
+<section class="macro-tool-form-container">
+    <?php if( have_posts() ): 
+        while ( have_posts() ):
+            the_post();?> 
 
-<?php if( have_posts() ): 
-    while ( have_posts() ):
-        the_post();?> 
-
+        <div class="widget-title">
             <h2><?php the_title(); ?></h2> 
+            <hr class="header-line">
+        </div>
 
 
-<?php the_content(); ?>
-    
-            <?php endwhile; ?> 
+    <?php the_content(); ?>
+        
+                <?php endwhile; ?> 
 
-            <?php else : ?>
-                    <p>No posts found</p>
-            <?php endif; ?>
-
+                <?php else : ?>
+                        <p>No posts found</p>
+                <?php endif; ?>
+</section>
 
 
 <?php get_footer(); ?>
