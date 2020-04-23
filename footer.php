13a7c7e0df5a388f5c73785c3ed54c65ae5b5494
cdrosales <cara10denise@hotmail.com>
2020-04-22 23:35:52 -0400

    chore: add link for linked in icon

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
         <div class="footer-social-media">
                 <a href="https://www.instagram.com/ty.thetrainer/"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-instagram.svg'; ?>" class="icon-instagram"/></a>
                 <a href="https://www.facebook.com/thetycommunity"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-facebook.svg'; ?>" class="icon-facebook"/></a>
-                <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-linkedin.svg'; ?>" class="icon-linkedin"/></a>
+                <a href="https://www.linkedin.com/in/ty-the-trainer-8460b81a7"><img src="<?php echo get_template_directory_uri() . '/assets/icons/icon-linkedin.svg'; ?>" class="icon-linkedin"/></a>
         </div>
 
     </div>
