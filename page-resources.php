22bd44fba0771be615703fd926b660d594e65632
cdrosales <cara10denise@hotmail.com>
2020-04-23 00:09:49 -0400

    feat: add coming soon section t o resources page

--- page-resources.php
+++ page-resources.php
@@ -24,5 +24,16 @@
                 <?php endif; ?>
 </section>
 
+<section class="coming-soon-resources-container">
+
+    <div class="widget-title">
+            <h2>Coming Soon</h2> 
+            <hr class="header-line">
+    </div>
+
+    <p>Working on getting you more resources to help your fitness journey. Check back later!</p>
+
+    <img src="<?php echo get_template_directory_uri() . '/assets/images/resources-coming-soon-IMG.png'; ?>" class="resources-coming-soon-IMG"/>
+</section>
 
 <?php get_footer(); ?>
